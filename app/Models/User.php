540ab6c6659901a81b2d43c7b2c475b1368e343c
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-08-07 15:39:45 +0700

    Add Pagination and Form Fiter from Search for Admin List
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Request;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -44,11 +46,24 @@ class User extends Authenticatable
     ];
 
     static public function getAdmin(){
-        return self::select('users.*')
+        $return = self::select('users.*')
                             ->where('user_type','=',1)
-                            ->where('is_delete','=',0)
-                            ->orderBy('id','desc')
-                            ->get();
+                            ->where('is_delete','=',0);
+                            if(!empty(Request::get('name'))){
+                                $return = $return->where('name','like','%'.Request::get('name').'%');
+                            }
+
+                            if(!empty(Request::get('email'))){
+                                $return = $return->where('email','like','%'.Request::get('email').'%');
+                            }
+
+                            if(!empty(Request::get('date'))){
+                                $return = $return->whereDate('created_at','=',Request::get('date'));
+                            }
+
+        $return = $return   ->orderBy('id','desc')
+                            ->paginate(2);
+        return $return;
     }
 
     static public function getSingle($id){
--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -7,7 +7,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Admin List</h1>
+            <h1>Admin List (Total :{{$getRecord->total()}})</h1>
           </div>
           <div class="col-sm-6" style="text-align:right;">
             <a href="{{url('admin/admin/add')}}" class="btn btn-primary">Add New Admin</a>
@@ -16,11 +16,45 @@
       </div><!-- /.container-fluid -->
     </section>
 
+    
+
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
         <div class="row">
           <div class="col-md-12">
+              <!-- left column -->
+                <!-- general form elements -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Search Admin</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <!-- form start -->
+                  <form method="get" action="">
+                    <div class="card-body">
+                      <div class="row">
+                      <div class="form-group col-md-3">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input type="text" class="form-control" value="{{Request::get('name')}}"  name="name" placeholder="Enter name">
+                      </div>
+                      <div class="form-group col-md-3">
+                        <label for="exampleInputPassword1">Email</label>
+                        <input type="text" class="form-control" value="{{Request::get('email')}}" name="email" placeholder="Enter email">
+                      </div>
+                      <div class="form-group col-md-3">
+                        <label>Date</label>
+                        <input type="date" class="form-control" value="{{Request::get('date')}}" name="date" placeholder="Enter email">
+                      </div>
+                      <div class="form-group col-md-3">
+                        <button class="btn btn-primary" type="submit" style="margin-top:30px;">Search</button>
+                        <a href="{{url('admin/admin/list')}}"class="btn btn-success" style="margin-top:30px;">Clear</a>
+                      </div>
+                    </div>
+                    </div>
+                    <!-- /.card-body -->
+                  </form>
+                </div>
 
             @include('_message')
 
@@ -46,7 +80,7 @@
                       <td>{{$value->id}}</td>
                       <td>{{$value->name}}</td>
                       <td>{{$value->email}}</td>
-                      <td>{{$value->created_at}}</td>
+                      <td>{{date('d-m-Y H:i A',strtotime($value->created_at))}}</td>
                       <td>
                         <a href="{{url('admin/admin/edit/'.$value->id)}}"class="btn btn-primary">Edit</a>
                         @if ($value->id != 1)
@@ -57,6 +91,9 @@
                     @endforeach 
                   </tbody>
                 </table>
+                <div style="padding:10px;float:right;">
+                  {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links()!!}
+                </div>
               </div>
               <!-- /.card-body -->
             </div>
